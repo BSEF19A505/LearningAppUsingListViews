d41c2b243c679e9c93013e2649c5577aba6d9819
bsef19a505 <bsef19a505@pucit.edu.pk>
2022-08-01 16:11:22 +0500

    Layout Done

--- app/src/main/java/com/example/multiplicationtableusinglistviewsandseekbar/MainActivity.java
+++ app/src/main/java/com/example/multiplicationtableusinglistviewsandseekbar/MainActivity.java
@@ -13,9 +13,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    SeekBar seek;
-    ListView listView;
-    TextView textView;
+
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -23,41 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView=findViewById(R.id.ListView);
 
-        seek=findViewById(R.id.seekBar);
-
-        seek.setMax(20);
-        seek.setMin(1);
-
-        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-                table(i);
-            }
-
-
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
     }
-    public void table(int num){
-        textView=findViewById(R.id.textView7);
-        ArrayList<String> rows=new ArrayList<>();
-        for(int i=1;i<=10;i++){
-            rows.add(num+" x "+i+" = "+(num*i));
-        }
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,rows);
-        listView.setAdapter(adapter);
-        textView.setText("Table of "+num);
-    }
+
 }
